1463a5b7d47041be721c548d232e73dfdbdc7ed5
jiroj <jirojbright1201@gmail.com>
2026-06-29 06:30:01 +0700

    icons: white background (composite over transparent); bump app SW caches v2

--- money/sw.js
+++ money/sw.js
@@ -1,5 +1,5 @@
 // Money PWA service worker (scope: /money/)
-const CACHE = 'money-v1';
+const CACHE = 'money-v2';
 const CORE = [
   './', './index.html', './manifest.webmanifest',
   '../app.css', '../shared/boot.js',
