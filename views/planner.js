5c1c7542ed6102506e0804f0462c5508eea34b56
jiroj <jirojbright1201@gmail.com>
2026-08-04 04:52:11 +0700

    planner: แก้ label หมวด mcdonalds ใน Time breakdown จาก Fast food เป็น Work (McDonald's)

--- views/planner.js
+++ views/planner.js
@@ -31,7 +31,7 @@ window.PlannerView = (function () {
     default:  F('<path d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3H18V1"/>') // mdi-calendar-blank
   };
   const ICON_LABELS = {
-    mcdonalds: 'Fast food', work: 'Work', sleep: 'Sleep', exercise: 'Exercise', read: 'Reading',
+    mcdonalds: "Work (McDonald's)", work: 'Work', sleep: 'Sleep', exercise: 'Exercise', read: 'Reading',
     clean: 'Cleaning', doc: 'Errands', video: 'Content', food: 'Food', game: 'Gaming', shower: 'Shower', default: 'Other'
   };
   function eventIconKey(e) {
